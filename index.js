7d0a9486b798f00e54694b52f4e252f3e1149b82
Shubhamparekh04 <shubhamparekh04@gmail.com>
2025-03-01 18:46:20 +0530

    Movie project

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(cookieParser());
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
 
 app.use("/", require("./router/movieRouter"));
 
